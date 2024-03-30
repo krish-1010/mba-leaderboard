130cefe8f4793a1798300eb021f080363ec7a2dc
krishna <krishnamchan@gmail.com>
2024-03-30 09:12:06 +0530

    commit

---
+++ src/app/teamdata.js
@@ -0,0 +1,8 @@
+export const teamdata = [
+  { tid: 1, tpoints: 162, tname: "Eagles" },
+  { tid: 2, tpoints: 243, tname: "Tigers" },
+  { tid: 3, tpoints: 157, tname: "Lions" },
+  { tid: 4, tpoints: 220, tname: "Wolves" },
+  { tid: 5, tpoints: 279, tname: "Hawks" },
+  { tid: 6, tpoints: 203, tname: "Panthers" },
+];
